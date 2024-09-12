f7b007f0cb684b74bbe779135b1b058dc1fcd283
Doni Nurramdan <doni.nurramdan@indocyber.id>
2024-09-12 14:19:31 +0700

    add dashboard view

---
+++ frontend/src/viewes/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container>
+      <h1>Dashboard</h1>
+      <!-- <v-btn @click="increment">Count is: {{ count }}</v-btn> -->
+      <router-link to="/vendor">List Vendor</router-link>
+    </v-container>
+  </template>
+  
+  <script lang="ts" setup>
+  import { useCounterStore } from '../stores/counter'
+  import { ref } from 'vue'
+  
+  const store = useCounterStore()
+  const count = ref(store.count)
+  
+  const increment = () => {
+    store.increment()
+    count.value = store.count
+  }
+  </script>
+  
