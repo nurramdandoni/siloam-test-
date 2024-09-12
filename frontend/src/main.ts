19081314412bbc8c39b27398eddddc15ba8b47b1
Doni Nurramdan <doni.nurramdan@indocyber.id>
2024-09-12 14:21:08 +0700

    setting main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
 import App from './App.vue'
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles' // Global CSS has to be imported
+// import '@mdi/font/css/materialdesignicons.css' // Material Design Icons
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import router from './router/router'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
