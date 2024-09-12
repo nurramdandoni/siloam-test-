565e4de96b286e50b295b114f10d97239c1e3d15
Doni Nurramdan <doni.nurramdan@indocyber.id>
2024-09-12 14:19:39 +0700

    add vendor view

---
+++ frontend/src/viewes/Vendor.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container>
+      <h1>Vendor</h1>
+      <!-- <v-btn @click="increment">Count is: {{ count }}</v-btn> -->
+      <router-link to="/dashboard">Dashboard</router-link>
+    </v-container>
+  </template>
+  
+  <script lang="ts" setup>
+  import { useCounterStore } from '../stores/counter'
+  import { ref } from 'vue'
+  
+  const store = useCounterStore()
+  const count = ref(store.count)
+  
+  const increment = () => {
+    store.increment()
+    count.value = store.count
+  }
+  </script>
+  
