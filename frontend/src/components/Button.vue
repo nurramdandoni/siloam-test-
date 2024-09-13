a1d846b2bdb5a66ca98e86b9692e8868d7919de6
Doni Nurramdan <doni.nurramdan@indocyber.id>
2024-09-13 16:31:36 +0700

    add componenet button

---
+++ frontend/src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-btn :prepend-icon="icon" :style="{ color: fontColor, backgroundColor: bgColor }" block>{{ ButtonName }}</v-btn>
+    <!-- <v-btn :prepend-icon="icon">{{ ButtonName }}</v-btn> -->
+  </template>
+  
+  <script lang="ts" setup>
+   import { defineProps } from 'vue';
+    
+    const props = defineProps<{
+      ButtonName: string,
+      icon?: string,
+      fontColor?: string,
+      bgColor?: string,
+    }>();
+  
+  </script>
+  
