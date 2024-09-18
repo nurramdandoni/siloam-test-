993378c0a2a6780e085b2abca6b64e0ee0eb2b06
Doni Nurramdan <doni.nurramdan@indocyber.id>
2024-09-18 17:12:25 +0700

    delete sequelize configuration in backend_old

--- backend/sequelize-cli.ts
+++
@@ -1,8 +0,0 @@
-import { resolve } from 'path';
-
-module.exports = {
-  'config': resolve('src/config/database.ts'),
-  'migrations-path': resolve('src/migrations'),
-  'seeders-path': resolve('src/seeders'),
-  'models-path': resolve('src/models')
-};
