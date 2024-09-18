4034a4c969573c053e67c22355cc607c2137b7f2
Doni Nurramdan <doni.nurramdan@indocyber.id>
2024-09-18 12:07:16 +0700

    add models

---
+++ backend/src/models/User.ts
@@ -0,0 +1,24 @@
+import { Table, Column, Model, DataType } from 'sequelize-typescript';
+
+@Table
+class User extends Model {
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  name!: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  email!: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  password!: string;
+}
+
+export default User;
