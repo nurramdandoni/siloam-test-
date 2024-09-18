9cfd9c0fe91be4a9cc172e8a477007dad3e9900d
Doni Nurramdan <doni.nurramdan@indocyber.id>
2024-09-18 17:13:13 +0700

    update sequelize init json to module export

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,12 +1,27 @@
-const { Sequelize } = require('sequelize-typescript');
+const dotenv = require("dotenv");
 
-const sequelize = new Sequelize({
-  dialect: 'mysql',
-  host: 'localhost',
-  username: 'latihan1',
-  password: 'latihan1',
-  database: 'siloam',
-  models: [__dirname + '/../models'], // path ke models
-});
-
-module.exports = sequelize;
+dotenv.config();
+module.exports =
+{
+  "development": {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
